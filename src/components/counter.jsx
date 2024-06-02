499713f521f93d0b85c4e11632748e656efe89d8
lakshma <lakshmag@byteridge.com>
2024-06-01 19:24:35 -0700

    updated index.js file

---
+++ src/components/counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  state = {
+    count: 1,
+    arr: [7, 8, 9],
+  };
+  handleEvent = (product) => {
+    console.log(product);
+    this.setState({count : this.state.count+1});
+  }
+
+  render() {
+    console.log('props', this.props)
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return (
+      <div>
+        <span className={classes}>{this.Format()}</span>
+        <button onClick={ () => {this.handleEvent(5)}} className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.arr.map((ar) => ( 
+            <li key={ar} value={ar} >{ar}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  Format() {
+    const formatC = this.state.count;
+    return formatC === 0 ? "zero" : formatC;
+  }
+}

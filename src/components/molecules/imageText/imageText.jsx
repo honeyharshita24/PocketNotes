844264879b320cdba82d9da50c1805abea546551
honeyharshita24 <harshitha.sontika24@gmail.com>
2024-06-07 01:16:12 -0700

    updated imageText molecule

--- src/components/molecules/imageText/imageText.jsx
+++ src/components/molecules/imageText/imageText.jsx
@@ -6,12 +6,12 @@ class ImageText extends Component {
     state = {  } 
     render() { 
             return (
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' ,justifyContent: 'center', minHeight: '100vh'}}>
                     <div style ={{display: 'flex', justifyContent : 'center' , alignItems : 'center'}}>
                         <HomePageImage/>
                     </div>
                     <Typography1 styles ={{fontSize :'50px' , fontWeight :700 }}>Pocket Notes</Typography1>
-                    <Typography1 styles ={{fontSize :'22px' , fontWeight :500 }}>Send and receive messages without keeping your phone online.{"\n"}
+                    <Typography1 styles ={{fontSize :'22px' , fontWeight :500 , maxWidth : '660px'}}>Send and receive messages without keeping your phone online.
                     Use Pocket Notes on up to 4 linked devices and 1 mobile phone</Typography1>
                 </div>
             );
